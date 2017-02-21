8542d6c6caf333683a5c2d813deeee121c836a91
Colin McKean <colinmckeandigital@gmail.com>
2017-02-21 16:27:32 +0000

    add test to ensure userwallet has funds

---
+++ specs/UserTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import atm.*;
+
+public class UserTest {
+User user;
+Wallet wallet;
+Account account;
+
+  @Before
+  public void before(){
+    wallet = new Wallet();
+    account = new Account();
+    user = new User(wallet, account);
+  }
+  
+  @Test
+  public void testUserWalletHasFunds(){
+    assertEquals(0, user.getWalletFunds());
+  }
+}
