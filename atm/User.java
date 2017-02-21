e39569c8e0a408541ec36ef52aeeff6230a4bcc1
Colin McKean <colinmckeandigital@gmail.com>
2017-02-21 16:29:14 +0000

    pass user tests

---
+++ atm/User.java
@@ -0,0 +1,18 @@
+package atm;
+public class User{
+  
+private Wallet wallet;
+private Account account;
+
+  public User(Wallet wallet, Account account){
+    this.wallet = wallet;
+    this.account = account;
+  }
+
+  public int getWalletFunds(){
+    return this.wallet.getTotalFunds();
+  }
+  public int getAccountBalance(){
+    return this.account.getBalance();
+  }
+}
